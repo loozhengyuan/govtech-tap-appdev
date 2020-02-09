bcd6d990ec3720fb7de0175fa1b11fc872db0705
Loo Zheng Yuan <loozhengyuan@users.noreply.github.com>
2020-02-09 21:26:10 +0800

    combine filters to fix buggy filters, likely due to using multiple filter methods on the same queryset

--- govgrant/api/views.py
+++ govgrant/api/views.py
@@ -22,35 +22,37 @@ class HouseholdViewSet(viewsets.ModelViewSet):
         on the qualifying criteria.
         """
 
+        # Construct filters
+        filters = {}
+
+        # Filters based on aggregate household income
+        min_income = self.request.query_params.get('min_income')
+        if min_income:
+            filters["household_income__gt"] = min_income
+        max_income = self.request.query_params.get('max_income')
+        if max_income:
+            filters["household_income__lt"] = max_income
+
+        # Filters based on members' age
+        min_age = self.request.query_params.get('min_age')
+        if min_age:
+            filters["members__dob__lt"] = datetime.date.today() - datetime.timedelta(days=int(min_age) * 365)
+        max_age = self.request.query_params.get('max_age')
+        if max_age:
+            filters["members__dob__gt"] = datetime.date.today() - datetime.timedelta(days=int(max_age) * 365)
+
+        # TODO: Consider using a better logic that checks
+        # if the spouse is related to one another within
+        # the same household.
+        if self.request.query_params.get('with_spouse'):
+            filters["spouse_count__gte"] = 2
+
         # TODO: Using timedelta is not accurate because it does not
         # take leap years into consideration. Consider using the
         # dateutil.relativedelta module instead.
         queryset = Household.objects.annotate(
             household_income=Sum('members__annual_income'),
             spouse_count=Count(Case(When(members__spouse__isnull=False, then=1))),
-        )
-
-        # Filters based on aggregate household income
-        household_income_above = self.request.query_params.get('household_income_above')
-        if household_income_above:
-            queryset.filter(household_income__gt=household_income_above)
-        household_income_below = self.request.query_params.get('household_income_below')
-        if household_income_below:
-            queryset.filter(household_income__lt=household_income_below)
-
-        # Filters based on members' age
-        members_age_above = self.request.query_params.get('members_age_above')
-        if members_age_above:
-            queryset.filter(Count(Case(When(members__dob__lt=datetime.date.today() - datetime.timedelta(days=members_age_above * 365)))))
-        members_age_below = self.request.query_params.get('members_age_below')
-        if members_age_below:
-            queryset.filter(Count(Case(When(members__dob__gt=datetime.date.today() - datetime.timedelta(days=members_age_below * 365)))))
-
-        # FIXME: This uses a simplistic filtering method of checking
-        # if spouses exists in household but does not check if they
-        # are related to one another.
-        has_spouse = self.request.query_params.get('has_spouse')
-        if has_spouse:
-            queryset.filter(spouse_count__gte=2)
+        ).filter(**filters)
 
         return queryset
